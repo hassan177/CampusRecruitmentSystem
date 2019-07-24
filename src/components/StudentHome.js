755f7e5dd91cdde9056ff518dad119f342e3fa73
Hassan Irshad <hassanansari17@live.com>
2019-07-24 12:39:14 +0500

    Adjustments in Sidemenu and navbar

--- src/components/StudentHome.js
+++ src/components/StudentHome.js
@@ -4,57 +4,35 @@ import '../css/student_home.css'
 
 class StudentHome extends Component {
 
-    openSlideMenu = () => {
-        document.getElementById('menu').style.width = '250px';
-        document.getElementById('content').style.marginLeft = '250px';
-    }
-    closeSlideMenu = () => {
-        document.getElementById('menu').style.width = '0';
-        document.getElementById('content').style.marginLeft = '0';
-    }
+    // openSlideMenu = () => {
+    //     document.getElementById('menu').style.width = '250px';
+    //     document.getElementById('content').style.marginLeft = '250px';
+    // }
+    // closeSlideMenu = () => {
+    //     document.getElementById('menu').style.width = '0';
+    //     document.getElementById('content').style.marginLeft = '0';
+    // }
 
     render() {
         return (
             <div id='content'>
-                <span className='slide'>
-                    <a href='#' onClick={() => {this.openSlideMenu()}}>
-                        <i className='fa fa-bars'></i>
-                    </a>
-                </span>
                 <div id='menu' className='nav'>
-                    <a className='close' onClick={() => {this.closeSlideMenu()}}>
-                        <i className='fa fa-times'></i>   
-                    </a>
-                    <a href='#'>Home</a>
-                    <a href='#'>About</a>
+                    <a href='#'><i class="fa fa-user-circle"></i>Profile</a>
+                    <a href='#'><i class="fa fa-window-maximize"></i>Noticeboard</a>
+                    <a href='#'><i class="fa fa-users"></i>Companies</a>
+                    <a href="#"><i class="fa fa-sticky-note"></i>Jobs</a>
                 </div>
-                <h1>Hello World</h1>
+                <div className="navbar">
+                    <h3>Hassan Irshad</h3>
+                    <div className="nav-right">
+                        <img src='https://via.placeholder.com/150' width="55px" />
+                        <span>Logout</span>
+                    </div>
+                </div>
+                <div className='body'>
+                    <h1>Hello World</h1>
+                </div>
             </div>
-
-        //     <Sidebar.Pushable as={Segment}>
-        //     <Sidebar as={Menu} animation='overlay' icon='labeled' inverted vertical visible width='thin'>
-        //         <Menu.Item as='a'>
-        //             <Icon name='home' />
-        //             Home
-        //         </Menu.Item>
-        //         <Menu.Item as='a'>
-        //             <Icon name='gamepad' />
-        //             Games
-        //         </Menu.Item>
-        //         <Menu.Item as='a'>
-        //             <Icon name='camera' />
-        //             Channels
-        //     </Menu.Item>
-        //     </Sidebar>
-
-        //     <Sidebar.Pusher>
-        //         <Segment basic>
-        //             <Header as='h3'>Application Contentadasfasfakfakfjkancfkahcnhnhacnhalnhcajj</Header>
-        //             <Image src='/images/wireframe/paragraph.png' />
-        //         </Segment>
-        //     </Sidebar.Pusher>
-        // </Sidebar.Pushable>
-          
         )
     }
 }
